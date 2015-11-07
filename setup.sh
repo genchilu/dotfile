f6a9653c8c4c1cfccca32b69d3c6c90564e8b744
Genchi <genchilu@gmail.com>
2015-11-07 20:25:44 +0800

    Install oh-my-zsh through git clone

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ git clone https://github.com/scrooloose/nerdtree.git /root/.vim/bundle/nerdtree
 
 #change shell to zsh
 chsh -s /bin/zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"bira\"/g' ~/.zshrc
