71479d26412b3b829ee6241b34c0e3aa76abd794
Genchi <genchilu@gmail.com>
2015-11-07 16:49:14 +0800

    Init

---
+++ .vimrc
@@ -0,0 +1,8 @@
+call pathogen#infect()
+syntax enable
+filetype plugin on
+set number
+set tabstop=4
+set shiftwidth=4
+set colorcolumn=80
+map <C-n> :NERDTreeToggle<CR>
---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt-get install -y git zsh curl vim tmux
+
+#vim plugin
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/scrooloose/nerdtree.git /root/.vim/bundle/nerdtree
+
+#change shell to zsh
+chsh -s /bin/zsh
