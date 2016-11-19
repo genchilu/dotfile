54920f4231a09124c689c8df53612e83d74381b2
Genchi <genchilu@gmail.com>
2016-11-20 00:14:29 +0800

    Add makefile, install epel while setup centos

--- basic-setup.sh
+++ basic-setup.sh
@@ -3,7 +3,7 @@ if [ -n "$(command -v apt-get)" ]; then
 	sudo apt-get install -y zsh curl vim tmux git htop
 elif [ -n "$(command -v yum)" ]; then
 	sudo yum update -y
-	sudo yum install -y zsh curl vim tmux git htop
+	sudo yum install -y zsh curl vim tmux git htop epel-release
 else
 	echo "can't find package manager!"
 	exit
@@ -15,6 +15,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 #change shell to zsh
+echo "type user's password to change shell to zsh"
 chsh -s /bin/zsh
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
---
+++ makefile
@@ -0,0 +1,8 @@
+basic:
+	bash basic-setup.sh
+
+python: basic
+	bash python-setup.sh
+
+golang: basic
+	bash golang-setup.sh
